44473e647897996b25b7c8f4d3f8841d5ca6e343
lyem <rfael403@gmail.com>
2024-05-02 17:02:08 -0300

    Code refactor

--- main.py
+++ main.py
@@ -1,70 +1,40 @@
-import requests
-from PIL import Image
 import os
 import re
-from colors import bcolors
-from colorama import init
-from termcolor import cprint
 import math
+import logging
+import requests
+from PIL import Image
+from colorama import init
+from colors import bcolors
+from termcolor import cprint
+import logging.handlers as handlers
 from fake_useragent import UserAgent
 from requests.adapters import HTTPAdapter, Retry
-import logging
-import logging.handlers as handlers
 
+# Configuração de log
+init()
 logger = logging.getLogger('app')
 logger.setLevel(logging.INFO)
-
 logHandler = handlers.RotatingFileHandler('app.log', maxBytes=500, backupCount=2)
 logHandler.setLevel(logging.INFO)
 logger.addHandler(logHandler)
 
-init()
-ua = UserAgent()
-
+# Configuração PIL
 Image.MAX_IMAGE_PIXELS = 933120000
 
-cprint(f"{bcolors.OKBLUE}Digite o id do manga: {bcolors.END}")
-id_manga = input()
-
+# Configuração requests
+r = requests.session()
+retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
+r.mount('https://', HTTPAdapter(max_retries=retries))
 base = 'https://tsuki-mangas.com'
 cdns = ['https://cdn.tsuki-mangas.com/tsuki','https://cdn2.tsuki-mangas.com']
-
+ua = UserAgent()
 headers = {'referer': f'{base}', 'user-agent': ua.random}
 
-r = requests.session()
-retries = Retry(total=5, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
-r.mount('https://', HTTPAdapter(max_retries=retries))
-
-data = r.get(f'{base}/api/v3/chapters?manga_id={id_manga}', headers=headers).json()
-
-chapters = []
-
-for i in range(int(data['lastPage'])):
-    response = r.get(f'{base}/api/v3/chapters?manga_id={id_manga}&order=desc&page={i + 1}', headers=headers).json()
-    chapters.extend(response['data'])
-
-for ch in chapters:
-    print(ch['number'])
-cprint(f'Selecione os caps \n{bcolors.BOLD}EXEMPLO: 1,2,3...{bcolors.END} \nou \n{bcolors.BOLD}EXEMPLO: 1-10{bcolors.END}')
-chs_selects = input()
-cprint(f'{bcolors.OKBLUE}Qual é o volume? (se não tiver é só apertar enter):{bcolors.END} ')
-vol = str(input() or '')
-if(vol != ''):
-    vol = f' (v{vol})'
-chs = chs_selects.split(',')
-if len(chs) == 1:
-    chs = chs_selects.split('-')
-    if len(chs) > 1:
-        nmin = chs[0]
-        nmax = chs[1]
-        chs = []
-        for ch in chapters:
-            if float(ch['number']) >= float(nmin) and float(ch['number']) <= float(nmax):
-                chs.append(ch['number'])
-
-for ch in chs:
+# Função para baixar páginas do capítulo
+def download_pages(chapters, ch, vol):
     for c in chapters:
-        if(float(ch) == float(c['number'])):
+        if float(ch) == float(c['number']):
             n = 1
             for version in c['versions']:
                 for scan in version['scans']:
@@ -77,27 +47,27 @@ for ch in chs:
                 cprint(f'{bcolors.OKBLUE}Selecione a versão: {bcolors.OKBLUE}')
                 version = int(input())
 
-            version_id = c['versions'][version-1]['id']
+            version_id = c['versions'][version - 1]['id']
 
             pages = r.get(f'{base}/api/v3/chapter/versions/{version_id}', headers=headers).json()
 
             manga_name = (pages['chapter']['manga']['title'][:20]) if len(pages['chapter']['manga']['title']) > 20 else pages['chapter']['manga']['title']
-            manga_name = re.sub('[^a-zA-Z0-9&_áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ-]','', manga_name)
+            manga_name = re.sub('[^a-zA-Z0-9&_áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ-]', '', manga_name)
             ch_title = ''
-            if(pages['chapter']['title']): 
-                ch_title = " ({0})".format(re.sub('[^a-zA-Z0-9&_áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ\s\.-]','', pages['chapter']['title']))
+            if(pages['chapter']['title']):
+                ch_title = " ({0})".format(re.sub('[^a-zA-Z0-9&_áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ\s\.-]', '', pages['chapter']['title']))
             pages = pages['pages']
             ch = c['number']
             g = []
-            if(len(c['versions'][version-1]['scans']) > 0):
-                for scan in c['versions'][version-1]['scans']:
+            if(len(c['versions'][version - 1]['scans']) > 0):
+                for scan in c['versions'][version - 1]['scans']:
                     g.append(re.sub(' ', '', scan['scan']['name']))
                     g.append('+')
                 g.pop()
             groups = "".join(g)
 
             if(bool(re.search("^0{1}\d", ch))):
-                ch = re.sub('0','',ch)
+                ch = re.sub('0', '', ch)
 
             cprint(f'{bcolors.WARNING}baixando cap {ch}{bcolors.END}')
             if not os.path.isdir(os.path.join('MangaDownloads', manga_name, f'{manga_name} [pt-br] - c{ch}{vol}{ch_title} [{groups}]')):
@@ -117,13 +87,13 @@ for ch in chs:
                             if(height > 10000):
                                 top = 0
                                 left = 0
-                                slices = int(math.ceil(height/5000))
+                                slices = int(math.ceil(height / 5000))
                                 count = 1
                                 for slice in range(slices):
                                     if count == slices:
                                         bottom = height
                                     else:
-                                        bottom = int(count * 5000)  
+                                        bottom = int(count * 5000)
 
                                     box = (left, top, width, bottom)
                                     img_slice = img.crop(box)
@@ -146,3 +116,40 @@ for ch in chs:
                     cprint(f'{bcolors.FAIL}falha ao baixar pagina {page_number} do cap {c["number"]}{bcolors.END}')
                     logger.info(f"falha ao baixar pagina {page_number} do cap {c['number']}{bcolors.END} - {cdn}{page['url']}")
                     page_number += 1
+
+# Início do programa
+if __name__ == "__main__":
+    cprint(f"{bcolors.OKBLUE}Digite o id do manga: {bcolors.END}")
+    id_manga = input()
+
+    data = r.get(f'{base}/api/v3/chapters?manga_id={id_manga}', headers=headers).json()
+
+    chapters = []
+    for i in range(int(data['lastPage'])):
+        response = r.get(f'{base}/api/v3/chapters?manga_id={id_manga}&order=desc&page={i + 1}', headers=headers).json()
+        chapters.extend(response['data'])
+
+    for ch in chapters:
+        print(ch['number'])
+
+    cprint(f'Selecione os caps \n{bcolors.BOLD}EXEMPLO: 1,2,3...{bcolors.END} \nou \n{bcolors.BOLD}EXEMPLO: 1-10{bcolors.END}')
+    chs_selects = input()
+
+    cprint(f'{bcolors.OKBLUE}Qual é o volume? (se não tiver é só apertar enter):{bcolors.END} ')
+    vol = str(input() or '')
+    if vol != '':
+        vol = f' (v{vol})'
+    
+    chs = chs_selects.split(',')
+    if len(chs) == 1:
+        chs = chs_selects.split('-')
+        if len(chs) > 1:
+            nmin = chs[0]
+            nmax = chs[1]
+            chs = []
+            for ch in chapters:
+                if float(ch['number']) >= float(nmin) and float(ch['number']) <= float(nmax):
+                    chs.append(ch['number'])
+
+    for ch in chs:
+        download_pages(chapters, ch, vol)
